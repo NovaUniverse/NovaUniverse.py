c1a4014560c32a56afd0618dda9ddfb4d9ae1d32
Dev Goldy <jassim7256@gmail.com>
2022-11-19 23:30:48 +0000

    midnight commit

--- novauniverse/interfaces/news/newsletter.py
+++ novauniverse/interfaces/news/newsletter.py
@@ -1,12 +1,15 @@
 from .. import InterfaceObject
 
+from ...utils.timestamp import Timestamp
+
 class NewsLetter(InterfaceObject):
     """A newsletter object but ."""
     def __init__(self, data:dict) -> None:
         self.__data:dict = data
 
         super().__init__(self, [
-            ("title", self.title)
+            ("title", self.title),
+            ("timestamp", self.timestamp)
         ])
 
     @property
@@ -15,5 +18,13 @@ class NewsLetter(InterfaceObject):
         return self.__data["title"]
 
     @property
-    def timestamp(self) -> str:
+    def timestamp(self) -> Timestamp:
         """Returns timestamp as python datetime object."""
+        return Timestamp(self.__data["timestamp"])
+
+    @property
+    def html(self) -> str:
+        """Returns the html of this letter as a string."""
+        return self.__data["html"]
+
+    
--- novauniverse/utils/timestamp.py
+++ novauniverse/utils/timestamp.py
@@ -1,6 +1,30 @@
 import datetime
 
 class Timestamp():
+    """A NovaUniverse API timestamp object."""
     def __init__(self, timestamp_dict:dict) -> datetime.datetime:
-        """Returns timestamp as python datetime object."""
-        
+        """Returns timestamp as python datetime object and more."""
+        self.__date:str = timestamp_dict["date"]
+        self.__timezone_type:int = timestamp_dict["timezone_type"]
+        self.__timezone:str = timestamp_dict["timezone"]
+
+    def __repr__(self) -> str:
+        return (
+            f'{self.__class__.__name__}' + 
+            f'(date={self.date}, timezone_type={self.timezone_type}, timezone={self.timezone})'
+        )
+
+    @property
+    def date(self) -> datetime.datetime:
+        return datetime.datetime.strptime(
+            self.__date, 
+            "%Y-%m-%d %H:%M:%S.%f"
+        )
+
+    @property
+    def timezone_type(self) -> int:
+        return self.__timezone_type
+
+    @property
+    def timezone(self) -> str:
+        return self.__timezone
