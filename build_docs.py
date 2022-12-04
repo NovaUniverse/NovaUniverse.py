8d3c410b03ce9530cc05cb1dfb5240226ee0380b
Dev Goldy <jassim7256@gmail.com>
2022-12-04 03:06:11 +0000

    doc updates

--- build_docs.py
+++ build_docs.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import pdoc
 
-GITHUB_RAW = "https://raw.githubusercontent.com/NovaUniverse/NovaUniverse.py/Dev_Goldy"
+GITHUB_RAW = "https://raw.githubusercontent.com/NovaUniverse/NovaUniverse.py/v2"
 
 pdoc.render.configure(logo=f"{GITHUB_RAW}/assets/logo.png", logo_link="https://novauniversepy.devgoldy.me/", footer_text="Copyright (C) 2022 - Dev Goldy", 
 template_directory=Path("./docs_template"))
