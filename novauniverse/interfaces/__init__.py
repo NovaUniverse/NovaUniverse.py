9f574c9285456f47b860d5d6825010789775da5d
Dev Goldy <jassim7256@gmail.com>
2023-03-21 14:41:00 +0000

    started implementing search interface

--- novauniverse/interfaces/__init__.py
+++ novauniverse/interfaces/__init__.py
@@ -1,4 +1,11 @@
+
+from abc import ABC, abstractmethod
+from typing import List
+from devgoldyutils import LoggerAdapter
+
+from .. import nova_logger
 from ..api import NovaAPI, Endpoints
+from ..utils.search import Search, SearchBy
 
 class Interface():
     """The base interface where all NovaUniverse.py endpoint interfaces inherit from."""
@@ -11,3 +18,44 @@ class BasicInterface(Interface):
 
     def __init__(self) -> None:
         super().__init__()
+
+
+# TODO: Finish this off..
+class SearchInterface(ABC, Interface):
+    """Adds searching to a basic NU.PY interface. Use this to add searching functionality to your interfaces."""
+    def __init__(self, supports:List[SearchBy]) -> None:
+        self.__supports = supports
+
+        self.logger = LoggerAdapter(nova_logger, prefix="SearchInterface")
+        super().__init__()
+
+    @abstractmethod
+    def find(self, search:Search, object_list=None) -> InterfaceObject|None:
+        if isinstance(search, Search):
+            if not search.search_by in self.__supports:
+                search.not_supported(self.__interface_class)
+            
+            # Search by id
+            # --------------
+            if search.search_by is SearchBy.id:
+                self.logger.debug(f"Searching for '{self.__interface_class.__name__}' by id...")
+
+                for object in object_list:
+                    if object.id == search.get_query():
+                        self.logger.info(f"Found '{self.__interface_class.__name__}' by id.")
+                        return object
+
+            # Search by name
+            # --------------
+            if search.search_by is SearchBy.name_:
+                self.logger.debug(f"Searching for '{self.__interface_class.__name__}' by name...")
+
+                for object in object_list:
+                    if object.name == search.get_query():
+                        self.logger.info(f"Found '{self.__interface_class.__name__}' by name.")
+                        return object
+        
+        else:
+            raise NovaError(f"You must use the 'novauniverse.Search()' class for searching in '{self.__interface_class.__name__}'.")
+
+        return None
---
+++ novauniverse/utils/search.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+from enum import Enum
+from typing import Type
+
+from ..errors import NovaError
+
+# SearchBy Enum class
+# ---------------------
+class SearchBy(Enum):
+    id = 1
+    name_ = 2
+
+# Errors
+# --------
+class SearchGotNoArgs(NovaError):
+    def __init__(self) -> None:
+        super().__init__("'Search()' class must have either id or name passed. Like --> Search(name='UwU Dev Goldy')")
+
+class SearchNotCompletelySupported(NovaError):
+    def __init__(self, searched_by, interface:object) -> None:
+        super().__init__(f"Searching by '{searched_by}' not supported by '{interface.__class__.__name__}' interface/endpoint.")
+
+"""
+class HasNotBeenSearched(NovaError):
+    def __init__(self) -> None:
+        super().__init__("Can't return this property/method because you have not searched for this object.", ErrorType.ERROR)
+"""
+
+# Search class
+# ---------------
+class Search():
+    """Search by id or name if supported."""
+    def __init__(self, id:str|int=None, name:str=None) -> None:
+        self.__id = id
+        self.__name = name
+
+        self.__using_id = False
+        self.__using_name = False
+
+        if not self.__id == None: self.__using_id = True
+        if not self.__name == None: self.__using_name = True
+        
+        if self.__using_id == False and self.__using_name == False:
+            raise SearchGotNoArgs()
+
+    def get_query(self) -> str|int|None:
+        if self.search_by is SearchBy.id:
+            return self.__id
+        if self.search_by is SearchBy.name_:
+            return self.__name
+        return None
+
+    @property
+    def search_by(self) -> SearchBy|None:
+        if self.__using_id:
+            return SearchBy.id
+        if self.__using_name:
+            return SearchBy.name_
+
+        return None
+
+    def not_supported(self, interface: object):
+        """Raises error to warn user this interface does not support searching by id/name."""
+        raise SearchNotCompletelySupported(self.search_by.name, interface)
