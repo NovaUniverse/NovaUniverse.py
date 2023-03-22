fee103e6f076df137786f0c3894cfd6130367350
Dev Goldy <jassim7256@gmail.com>
2023-03-22 18:19:05 +0000

    updated pyproject.toml

--- novauniverse/info.py
+++ novauniverse/info.py
@@ -1,7 +1,6 @@
 VER = 2.0
-STAGE = ("dev", 4)
 
-VERSION = f"v{VER}-{STAGE[0]}-{STAGE[1]}" # Full version string.
+VERSION = f"{VER}" # Full version string.
 
 PACKAGE_NAME= "NovaUniverse.py"
 PACKAGE_NAME_WITH_VER = f"{PACKAGE_NAME} ({VERSION})"
@@ -9,4 +8,4 @@ PACKAGE_NAME_WITH_VER = f"{PACKAGE_NAME} ({VERSION})"
 LOGGER_NAME = f"\u001b[31m{PACKAGE_NAME}\u001b[0m"
 """The name of the logger. You can use this to get the logger with ``logging.getLogger()``."""
 
-COPYRIGHT = "\nCopyright (c) 2022-present (Dev Goldy)"
+COPYRIGHT = "\nCopyright (c) 2023-present (Dev Goldy)"
